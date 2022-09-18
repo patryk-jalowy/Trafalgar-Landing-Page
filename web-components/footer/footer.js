73b0425c568097015c88a415db7a1f7f4567af53
Patryk Jalowy <patryk.jalowy@gmail.com>
2022-09-18 20:19:53 +0200

    added footer styling

--- web-components/footer/footer.js
+++ web-components/footer/footer.js
@@ -2,32 +2,40 @@ const foot = document.createElement('template')
 
 foot.innerHTML = `
 <style>
-@import "/web-components/footer/footer.css"/;
+@import "/web-components/footer/footer.css";
 
 </style>
 
 <div class="footer-container">
     <div class="row">
-        <div class="column-6">
+        <div class="col-4">
         </div>
-        <div class="column-3">
+        <div class="col-3">
             <h2></h2>
             <ul>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
+                <li slot name="first-1"></li>
+                <li slot name="second-1"></li>
+                <li slot name="third-1"></li>
+                <li slot name="fourth-1"></li>
             </ul>
         </div>
-        <div class="column-3">
+        <div class="col-3">
             <h2></h2>
                 <ul>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
+                <li slot name="first-2"></li>
+                <li slot name="second-2"></li>
+                <li slot name="third-2"></li>
+                <li slot name="fourth-2"></li>
                 </ul>
         </div>
+        <div class="col-3">
+        <h2></h2>
+            <ul>
+            <li slot name="first-3"></li>
+            <li slot name="second-3"></li>
+            <li slot name="third-3"></li>
+            <li slot name="fourth-3"></li>
+            </ul>
     </div>
 </div>
 `
